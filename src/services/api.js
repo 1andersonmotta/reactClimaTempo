5a59727fbbead6ac05dedb6dc9e046d21734b375
1andersonmotta <devandersonmotta@gmail.com>
2023-09-11 16:33:50 -0300

    feature api consumer

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+const key = 'f674ec32567d46acb75182157231109'
+
+const fetchData = async (city) => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=no`;
+
+    const fetchResponse = await fetch(url);
+    const data = await fetchResponse.json()
+    return data;
+}
+
+export default fetchData;
